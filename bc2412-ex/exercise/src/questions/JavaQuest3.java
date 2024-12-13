0b41a04e4403778b5a7880af336d0cb65a17c628
Jack.Choi <choisk0999@gmail.com>
2024-12-13 10:01:00 +0800

    20241213

--- bc2412-ex/exercise/src/questions/JavaQuest3.java
+++ bc2412-ex/exercise/src/questions/JavaQuest3.java
@@ -40,11 +40,13 @@ public class JavaQuest3 {
     
     if ( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
       numberOfDaysInMonth = 31;
-    } else if ( month != 2 ){
+    } else if ( month == 4 || month == 6 || month == 9 || month == 11){
       numberOfDaysInMonth = 30;
-    }else if (year%4 ==0 && year%100 !=0 || year%400 == 0){
+    }else if (month == 2 ){
+      if(year%4 ==0 && year%100 !=0 || year%400 == 0){
       numberOfDaysInMonth = 29;
     }else {numberOfDaysInMonth = 28;}
+    }
 
 
     System.out.println(
