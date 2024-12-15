ca84ab2f1b6d33475c470a9a96e67601a808f976
Jack.Choi <choisk0999@gmail.com>
2024-12-15 18:29:19 +0800

    20241215

--- bc2412-ex/exercise/src/questions/JavaQuest5.java
+++ bc2412-ex/exercise/src/questions/JavaQuest5.java
@@ -5,17 +5,21 @@ public class JavaQuest5 {
    */
   public static void main(String[] args) {
     String str = "coding bootcamp."; // You should not change this line
-    char target = 'c'; // Update this target to test the logic
+    char target = 'o'; // Update this target to test the logic
 
     // if not found, print "Not Found."
     // code here ...
-int beforeIndex = str.length();
-int lastIndex = 0;
-//int lastCharIndex = 0;
-for (int i = 0; i < str.length(); i++){
-  if (str. charAt(i) == target && i > beforeIndex){lastIndex = i;
-  }else if(str.charAt(i)== target ){beforeIndex = i;}}
-System.out.println("The index of the last character of c is " + lastIndex);
-System.out.println(beforeIndex);
+    int lastIndex = 0;
+    int firstIndex = str.length();
+    boolean isTargetExist = false;
+
+    for ( int i = 0; i < str.length(); i++){
+      if ( str.charAt(i) == target && i < firstIndex){firstIndex = i; isTargetExist = true;}
+      else if ( str.charAt(i) == target && i > firstIndex){
+         lastIndex = i; isTargetExist = true;}
+      }
+    if (isTargetExist == true){System.out.println("The index of the last character of c is " + lastIndex);}
+    else if (isTargetExist == false){System.out.println("Not Found.");}
+    
   }
 }
