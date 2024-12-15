7acf4a4b0e8bd0817973a42bfe0a40cf6dd50387
Jack.Choi <choisk0999@gmail.com>
2024-12-15 18:51:48 +0800

    20241215

--- bc2412-ex/exercise/src/questions/JavaQuest6.java
+++ bc2412-ex/exercise/src/questions/JavaQuest6.java
@@ -8,9 +8,10 @@ public class JavaQuest6 {
     int first = 0, second = 1;
     // for loop to print first 15 numbers in Fibonacci Sequence
 
- for (int i = 0; i < 15; i++){
-if ( first < second ){ System.out.print( first + " " ); first = first + second;}
-else if ( first >= second ){ System.out.print( second + " " ); second = second + first;}}
+    for (int i = 0; i < 15; i++){
+      if (first < second){System.out.print(first + " "); first += second;}
+      else if ( second <= first){System.out.print(second + " "); second += first;}
+    }
 
   }
 }
