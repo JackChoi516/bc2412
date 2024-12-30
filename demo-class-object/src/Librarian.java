da964525b6dbe3b77956b35308bb85b8aa5e2c68
Jack.Choi <choisk0999@gmail.com>
2024-12-30 17:45:16 +0800

    20241230

--- demo-class-object/src/Librarian.java
+++ demo-class-object/src/Librarian.java
@@ -1,11 +1,16 @@
 public class Librarian {
   private Library library;
 
+
   public Librarian(Library library){
     this.library = library;
   }
 
   public void addBook(Book book){
     library.addBook(book);
+  }
+
+  public void removeByTitle(String title){
+
   }
 }
--- demo-class-object/src/Student.java
+++ demo-class-object/src/Student.java
@@ -1,7 +1,30 @@
 public class Student {
+  private int id;
   private int score;
-  
+  private int candies;
 
+  public Student(int id, int score){
+    this.id = id;
+    this.score = score;
+  }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public int getScore(){
+    return this.score;
+  }
+
+  public int getCandies(){
+    return this.candies;
+  }
+
+  public void receiveCandy(int nums){
+    this.candies += nums;
+  } 
+ 
+ 
   // Expected Output:
   // 20 candies > distribute students according to their score
   // Rule: Every round of distribution, Above 80 score > 2 candies; above 60 - 79 > 1 candy
@@ -29,4 +52,34 @@ public class Student {
   // Student 6 has 0 candies
 
   // Tips: static > candyCount (while loop)
+
+  public static void main(String[] args) {
+
+    Student s1 = new Student(1, 67);
+    Student s2 = new Student(2, 89);
+    Student s3 = new Student(3, 50);
+    Student s4 = new Student(4, 99);
+    Student s5 = new Student(5, 60);
+    Student s6 = new Student(6, 59);
+    Student[] sArr = new Student[]{s1, s2, s3, s4, s5, s6};
+
+    int candyCount = 20;
+    while(candyCount > 0){
+      for (int i = 0; i < sArr.length; i++){
+        if (sArr[i].getScore() >= 80 && candyCount >=2){
+          sArr[i].receiveCandy(2);
+          candyCount -= 2;
+        }else if (sArr[i].getScore() >= 60 && sArr[i].getScore() <= 79){
+          sArr[i].receiveCandy(1);
+          candyCount -= 1;
+        }else {
+          continue;
+        }
+      }
+    }
+
+    for (int i = 0; i < sArr.length; i++){
+      System.out.println(sArr[i].getId() + " " + sArr[i].getCandies());
+    }
+  }
 }
